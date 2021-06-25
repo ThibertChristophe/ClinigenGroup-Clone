2a11da82784f81f1a22afb9c23987fab52adfa5f
ThibertChristophe <c.thibert89@gmail.com>
2021-06-25 13:25:34 +0200

    modif slider et search du header
    css du search du header lors de la saisie

--- js/_slider.js
+++ js/_slider.js
@@ -9,26 +9,54 @@ const slider_dot_1 = document.querySelector(".slider_dot_1");
 const slider_dot_2 = document.querySelector(".slider_dot_2");
 const slider_dot_3 = document.querySelector(".slider_dot_3");
 
-// Affiche le slide 2
-setTimeout(() => {
-  // Slide1 => quitte l'ecran
-  slider1.style.left = "-50%";
-  // Slider2 => arrive dans l'écran
-  slider2.style.left = "50%";
-  slider2.style.transform = "translateX(-50%)";
-  // Change les dot du dessous => active le dot 2
-  slider_dot_1.style.background = "transparent";
-  slider_dot_2.style.background = "#fff";
-}, 10000);
+let slideActif = 1;
 
-// Affiche le slide 3
-setTimeout(() => {
-  // Slider2 => quitte l'ecran
-  slider2.style.left = "-50%";
-  // Slider3 => arrive dans l'écran
-  slider3.style.left = "50%";
-  slider3.style.transform = "translateX(-50%)";
-  // Change les dot du dessous => active le dot 3
+// Affiche le slide en parametre
+const afficheSlide = (slide) => {
+  slide.style.left = "50%";
+  slide.style.transform = "translateX(-50%)";
+};
+
+// Retire le slide en parametre
+const retireSLide = (slide) => {
+  slide.style.left = "-50%";
+};
+
+const defileSlide = () => {
+  // Premier défilement de slide 1 => 2
+  setTimeout(() => {
+    // Slide1 => quitte l'ecran
+    retireSLide(slider1);
+    // Slider2 => arrive dans l'écran
+    afficheSlide(slider2);
+    slideActif = 2;
+    // Change les dot du dessous => active le dot 2
+    slider_dot_1.style.background = "transparent";
+    slider_dot_2.style.background = "#fff";
+  }, 10000);
+
+  // Deuxieme défilement de slide 2 => 3
+  setTimeout(() => {
+    // Slider2 => quitte l'ecran
+    retireSLide(slider2);
+    // Slider3 => arrive dans l'écran
+    afficheSlide(slider3);
+    slideActif = 3;
+    // Change les dot du dessous => active le dot 3
+    slider_dot_2.style.background = "transparent";
+    slider_dot_3.style.background = "#fff";
+  }, 20000);
+};
+
+defileSlide();
+
+/**
+ * Click des dots
+ */
+slider_dot_1.addEventListener("click", () => {
+  console.log("ok");
+
+  slider_dot_3.style.background = "transparent";
   slider_dot_2.style.background = "transparent";
-  slider_dot_3.style.background = "#fff";
-}, 20000);
+  slider_dot_1.style.background = "#fff";
+});
