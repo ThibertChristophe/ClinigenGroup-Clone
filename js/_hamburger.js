0d2f8ae80257368fce6a458f9b6e1aa5e6697a57
ThibertChristophe <c.thibert89@gmail.com>
2021-07-06 10:02:17 +0200

    sous menu hamburger fini
    dans toute les pages le menu hamburger est complet

--- js/_hamburger.js
+++ js/_hamburger.js
@@ -2,11 +2,30 @@ const hamburgerButton = document.querySelector(".hamburger > button");
 const hamburgerButtonIMG = document.querySelector(".hamburger > button > img");
 const hamburger_menu = document.querySelector(".hamburger_menu");
 
+// Sous menu
 const hamburger_menu_link_shortage = document.querySelector(
   ".hamburger_menu_link_shortage"
 );
 
-const hamburger_sousMenu = document.querySelector(".hamburger_sousMenu");
+const hamburger_sousMenu_shortages = document.querySelector(
+  ".hamburger_sousMenu_shortages"
+);
+
+const hamburger_menu_link_help = document.querySelector(
+  ".hamburger_menu_link_help"
+);
+
+const hamburger_sousMenu_help = document.querySelector(
+  ".hamburger_sousMenu_help"
+);
+
+const hamburger_menu_link_about = document.querySelector(
+  ".hamburger_menu_link_about"
+);
+
+const hamburger_sousMenu_about = document.querySelector(
+  ".hamburger_sousMenu_about"
+);
 
 /**
  * Affiche / retire le menu lors du click sur le menu hamburger
@@ -25,5 +44,11 @@ hamburgerButton.addEventListener("click", () => {
  * Sous menu
  */
 hamburger_menu_link_shortage.addEventListener("click", () => {
-  hamburger_sousMenu.classList.toggle("show");
+  hamburger_sousMenu_shortages.classList.toggle("show");
+});
+hamburger_menu_link_help.addEventListener("click", () => {
+  hamburger_sousMenu_help.classList.toggle("show");
+});
+hamburger_menu_link_about.addEventListener("click", () => {
+  hamburger_sousMenu_about.classList.toggle("show");
 });
