23bf94ddc7ea89cbb724ba5e81de78d6e348b23c
ThibertChristophe <c.thibert89@gmail.com>
2021-07-23 14:42:47 +0200

    Formulaire fini avec champs mandatory et effet de cadre et libelle rouge en warining

--- public/js/_form.js
+++ public/js/_form.js
@@ -8,8 +8,13 @@ const listInput = document.querySelectorAll(".form_card > input");
 // On va vérifier les input
 btn_submit.addEventListener("click", () => {
   listInput.forEach((input) => {
+    const label_cache = input.parentNode.querySelector(".mustBeFill");
     if (input.value === "") {
       input.style.border = "1px solid red";
+      label_cache.style.display = "block";
+    } else {
+      input.style.border = "1px solid rgb(221, 221, 221)";
+      label_cache.style.display = "none";
     }
   });
 });
